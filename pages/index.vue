2ece4fa7ec9cf0bd4e66083846aa42345928df4c
yagisuke <yagisuke.0209@gmail.com>
2018-11-25 01:10:37 +0900

    lint

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,13 @@
       <h2 class="subtitle">Nuxt.js project</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/yagisuke/nuxt-circle-ci-demo" target="_blank" class="button--grey">GitHub</a>
+        <a
+          href="https://github.com/yagisuke/nuxt-circle-ci-demo"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   </section>
